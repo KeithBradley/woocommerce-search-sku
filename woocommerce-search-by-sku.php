710d72e904af67c960a44180f33ae3d7c81e54b5
kb <keith@logicdesign.co.uk>
2017-01-26 12:10:01 +0000

    Added comments to code

--- woocommerce-search-by-sku.php
+++ woocommerce-search-by-sku.php
@@ -29,26 +29,31 @@ function loki_woo_search_where( $where )
 {
 	global $pagenow, $wpdb, $wp;
 
+	// Storage array to keep product id's
 	$search_ids = array();
+
+	// Break search phrase into words
 	$terms	= explode(' ', $wp->query_vars['s'] );
 
-	// SEARCH _SKU
-	foreach ( $terms as $term ) {
-		$sku_to_id = $wpdb->get_results( $wpdb->prepare( "SELECT ID, post_parent FROM {$wpdb->posts} LEFT JOIN {$wpdb->postmeta} ON {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id WHERE meta_key='_sku' AND meta_value LIKE %s;", '%' . $wpdb->esc_like( wc_clean( $term ) ) . '%' ) );
+	// Loop through terms and compare with _sku meta_value
+	if ( ! empty($terms) ) {
+		foreach ( $terms as $term ) {
+			$sku_to_id = $wpdb->get_results( $wpdb->prepare( "SELECT ID, post_parent FROM {$wpdb->posts} LEFT JOIN {$wpdb->postmeta} ON {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id WHERE meta_key='_sku' AND meta_value LIKE %s;", '%' . $wpdb->esc_like( wc_clean( $term ) ) . '%' ) );
 
-		$sku_to_id = array_merge( wp_list_pluck( $sku_to_id, 'ID' ), wp_list_pluck( $sku_to_id, 'post_parent' ) );
+			$sku_to_id = array_merge( wp_list_pluck( $sku_to_id, 'ID' ), wp_list_pluck( $sku_to_id, 'post_parent' ) );
 
-		if ( sizeof( $sku_to_id ) > 0 ) {
-			$search_ids = array_merge( $search_ids, $sku_to_id );
+			if ( sizeof( $sku_to_id ) > 0 ) {
+				$search_ids = array_merge( $search_ids, $sku_to_id );
+			}
 		}
 	}
 
+	// If product ids have been found modify search query with id's
 	if ( ! empty($search_ids) ) {
 		$where = str_replace( 'AND (((', "AND ( ({$wpdb->posts}.ID IN (" . implode( ',', $search_ids ) . ")) OR ((", $where );
 	}
 
 	return $where;
-
 }
 add_filter( 'posts_where', 'loki_woo_search_where');
 
